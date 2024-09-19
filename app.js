91b8d8e03d3f41b4033a696cf0a0428906b190b2
jungdeepaksingh <43090779+jungdeepaksingh@users.noreply.github.com>
2024-09-19 15:58:35 +0545

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ HTTP SERVER OLUŞTURULDU
 
 */
 http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyriisd.');
+	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyriid.');
 });
 
 
